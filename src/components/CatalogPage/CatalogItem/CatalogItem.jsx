0bf2c1c41ae5e939ca6c40b0c76fc9cfe49f580e
Borys <ihorborys@gmail.com>
2026-02-21 20:26:33 +0200

    edited product image path

--- src/components/CatalogPage/CatalogItem/CatalogItem.jsx
+++ src/components/CatalogPage/CatalogItem/CatalogItem.jsx
@@ -17,7 +17,7 @@ const CatalogItem = ({product}) => {
         <section>
           <h4 className={styles.brand}>{product.brand}</h4>
           <p className={styles.unicode}>{product.unicode}</p>
-          <img src="/img/catalog/luk_602000700.jpg" alt="/img/catalog/no_item.png"
+          <img src="/img/catalog/luk_60200070.jpg" alt="/img/catalog/no_item.png"
                width="150px"/>
         </section>
 
